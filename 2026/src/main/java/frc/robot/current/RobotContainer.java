b4e6a6ab5daebf821cdf014d109f9a50762a1c68
keckbw <keckbw@gmail.com>
2026-02-28 14:12:03 -0500

    cleaned up unused imports

--- 2026/src/main/java/frc/robot/current/RobotContainer.java
+++ 2026/src/main/java/frc/robot/current/RobotContainer.java
@@ -7,8 +7,6 @@ package frc.robot.current;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
-import com.pathplanner.lib.pathfinding.Pathfinding;
 import com.pathplanner.lib.auto.NamedCommands;
 
 import edu.wpi.first.math.geometry.Pose2d;
